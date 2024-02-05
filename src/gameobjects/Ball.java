a5a5b80d72b86803e3ce3028846c1810aa6c5075
Yonatan Zhenin <yonatan.jenin@mail.huji.ac.il>
2024-02-05 17:15:53 +0200

    added ball and paddle classes

--- src/gameobjects/Ball.java
+++ src/gameobjects/Ball.java
@@ -1,2 +1,29 @@
-package gameobjects;public class Ball {
+package gameobjects;
+
+import danogl.GameObject;
+import danogl.collisions.Collision;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+
+public class Ball extends GameObject {
+
+    private int collisionCounter;
+
+
+    public Ball(Vector2 position, Vector2 size, Renderable image) {
+        super(position, size, image);
+        collisionCounter = 0;
+    }
+
+    @Override
+    public void onCollisionEnter(GameObject other, Collision collision) {
+        super.onCollisionEnter(other, collision);
+        collisionCounter++;
+        Vector2 newVelocity = getVelocity().flipped(collision.getNormal());
+        setVelocity(newVelocity);
+    }
+
+    public int getCollisionCounter() {
+        return collisionCounter;
+    }
 }
--- src/gameobjects/Paddle.java
+++ src/gameobjects/Paddle.java
@@ -1,2 +1,6 @@
-package gameobjects;public class Paddle {
+package gameobjects;
+
+public class Paddle {
+
+
 }
